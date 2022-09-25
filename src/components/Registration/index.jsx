dfe7415a9a842cfc020c5366f461b2eb251dbd49
BukovskaPavla <bukovskapavla@gmail.com>
2022-09-25 20:21:30 +0200

    component Registration, event on inputs and on the form.

--- src/components/Registration/index.jsx
+++ src/components/Registration/index.jsx
@@ -11,11 +11,41 @@ export const Registration = () => {
 
      const handleSubmit = (e) => {
         e.preventDefault()
-        alert("registrace proběhla")
-     }
-     const handleEmail = () => {
+        
+        if (heslo === hesloConf) {
+            alert(`Vítejte ${user.username}! Registrace proběhla dobře a váš účet je založen.`)
+            console.log(user)
+        } else {
+            alert("Zadaná hesla se neschodují")
+        }
     }
-         
+    
+    const [subEmail,setSubEmail] = useState("")
+    const handleEmail = (email) => {
+        setSubEmail(email.substring(0, email.indexOf("@")))
+        setUser({...user, username: subEmail, email: email })
+    }
+
+    const handleUserName = (uName) => {
+        if (uName !== "") {
+            setUser({...user, username: uName})
+        }
+
+    }
+
+    const [heslo, setHeslo] = useState()
+    const [hesloConf, setHesloConf] = useState()
+
+    const handlePassword = (pass) => {
+        setUser({...user, password: pass })  
+        setHeslo(pass) 
+    }
+
+    const handlePasswordConfirm = (passConf) => {
+        setUser({...user, passwordConfirm: passConf })
+        setHesloConf(passConf) 
+    }
+
     return (
     <>
       <h1>REGISTRATION</h1>
@@ -24,9 +54,9 @@ export const Registration = () => {
             <div id="inner-circle"></div>
         </div>
         <input type='email' placeholder='Email' className='email' autoFocus onChange={(e)=>handleEmail(e.target.value)}></input>
-        <input type='text' placeholder='User Name'className='name'></input>
-        <input type='password' placeholder='Password' className='password'></input>
-        <input type='password' placeholder='Password Confirm' className='confirmPassword'></input>
+        <input type='text' placeholder={subEmail==="" ? 'User Name':subEmail} className='name'onChange={(e)=> handleUserName(e.target.value)}></input>
+        <input type='password' placeholder='Password' className='password' onChange={(e) => handlePassword(e.target.value)}></input>
+        <input type='password' placeholder='Password Confirm' className='confirmPassword' onChange={(e) => handlePasswordConfirm(e.target.value)}></input>
         <button>REGISTER</button>
       </form>
     </>
